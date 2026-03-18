24393b6b12facfa0b8221f6dd860f00b21b5e6c4
Valentyn_Koval <J.K.play2015@gmail.com>
2026-03-18 15:18:53 +0200

    add a start menu with player name and game count input

--- src/main/java/com/game/bj/Main.java
+++ src/main/java/com/game/bj/Main.java
@@ -1,7 +1,22 @@
 package com.game.bj;
 
+import com.game.bj.dto.Player;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Black Jack");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter number of games:");
+        int numberOfGames = scanner.nextInt();
+
+        Player player = new Player(name);
+
+        System.out.println(String.format("Welcome %s, you have %d games to play", player.getName(), numberOfGames));
     }
 }
