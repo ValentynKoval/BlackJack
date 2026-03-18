ab186cd611957aabe278ffbca5caff71385c12f6
Valentyn_Koval <J.K.play2015@gmail.com>
2026-03-18 20:54:11 +0200

    add methods for computer score, hand, dealer hand, and game result calculation

--- src/main/java/com/game/bj/service/GameService.java
+++ src/main/java/com/game/bj/service/GameService.java
@@ -1,8 +1,12 @@
 package com.game.bj.service;
 
 import com.game.bj.dto.Card;
+import com.game.bj.dto.GameResult;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Stack;
 
 public class GameService {
     public int calculateScore(List<Card> hand) {
@@ -24,5 +28,45 @@ public class GameService {
             if (card.getCost() == 11) return true;
         }
         return false;
+    }
+
+    public int getComputerScore() {
+        return new Random().nextInt(17, 28);
+    }
+
+    public List<Card> getComputerHand(Stack<Card> deck) {
+        List<Card> computerHand = new ArrayList<>();
+        int numberOfCard = new Random().nextInt(2, 6);
+        for (int i = 0; i < numberOfCard; i++) {
+            computerHand.add(deck.pop());
+        }
+        return computerHand;
+    }
+
+    public List<Card> getDealerHand(DeckService deckService) {
+        List<Card> dealerHand = new ArrayList<>();
+        dealerHand.add(deckService.dealCard());
+        do {
+            dealerHand.add(deckService.dealCard());
+        } while (calculateScore(dealerHand) < 17);
+        return dealerHand;
+    }
+
+    public GameResult getGameResult(int pScore, int dScore) {
+        if (dScore > 21) {
+            if (pScore > 21) {
+                return GameResult.LOSE;
+            }
+            return GameResult.PLAYER;
+        } else {
+            if (pScore > 21) {
+                return GameResult.DEALER;
+            } else if (pScore > dScore) {
+                return GameResult.PLAYER;
+            } else if (dScore > pScore) {
+                return GameResult.DEALER;
+            }
+        }
+        return GameResult.DRAW;
     }
 }
