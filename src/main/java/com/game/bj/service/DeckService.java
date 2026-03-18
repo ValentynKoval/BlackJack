29057ed9f3519544250107e4e210838a7b0af221
Valentyn_Koval <J.K.play2015@gmail.com>
2026-03-18 12:12:10 +0200

    create deck service class for creating and managing a deck of cards

---
+++ src/main/java/com/game/bj/service/DeckService.java
@@ -0,0 +1,42 @@
+package com.game.bj.service;
+
+import com.game.bj.dto.Card;
+import com.game.bj.dto.Rank;
+import com.game.bj.dto.Suit;
+import com.game.bj.exception.DeckException;
+import lombok.SneakyThrows;
+
+import java.util.Stack;
+
+public class DeckService {
+    private Stack<Card> deck = new Stack<>();
+
+    @SneakyThrows
+    public void createDeck(int deckNumber) {
+        deck.clear();
+
+        if (deckNumber < 0 || deckNumber > 8) {
+            throw new DeckException("Number of deck must be between 1 to 8");
+        }
+
+        for(int i = 0; i < deckNumber; i++) {
+            for (Rank rank : Rank.values()) {
+                for (Suit suit : Suit.values()) {
+                    deck.add(new Card(rank, suit));
+                }
+            }
+        }
+    }
+
+    public Stack<Card> getDeck() {
+        return deck;
+    }
+
+    public int getCardCount() {
+        return deck.size();
+    }
+
+    public Card delaCard() {
+        return deck.pop();
+    }
+}
