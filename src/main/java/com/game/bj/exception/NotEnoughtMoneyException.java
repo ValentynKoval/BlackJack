813e9c9127fefdcf3dc08e0d8593acdda5507bfb
Valentyn_Koval <J.K.play2015@gmail.com>
2026-03-20 13:00:49 +0200

    create NotEnoughtMoneyException class for game currency validation

---
+++ src/main/java/com/game/bj/exception/NotEnoughtMoneyException.java
@@ -0,0 +1,7 @@
+package com.game.bj.exception;
+
+public class NotEnoughtMoneyException extends RuntimeException {
+    public NotEnoughtMoneyException(String message) {
+        super(message);
+    }
+}
