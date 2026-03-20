f3985b70be96a28759b8ef1f82fe8f369c81f58d
Valentyn_Koval <J.K.play2015@gmail.com>
2026-03-20 13:01:13 +0200

    add constructor for Player with amount and initialize amount field

--- src/main/java/com/game/bj/dto/Player.java
+++ src/main/java/com/game/bj/dto/Player.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +15,13 @@ public class Player {
     private String name;
     private int numberOfGames = 0;
     private int numberOfWins = 0;
+    private Amount amount;
 
     private List<Card> hand = new ArrayList<>();
 
-    public Player(String name) {
+    public Player(String name, BigInteger amount) {
         this.name = name;
+        this.amount = new Amount(amount);
     }
 
     public void incrementNumberOfGames() {
